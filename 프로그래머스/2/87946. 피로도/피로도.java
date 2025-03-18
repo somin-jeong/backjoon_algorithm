a301823114a3e527a38b566ab11875b2d6245ffd
somin-jeong <81250561+somin-jeong@users.noreply.github.com>
2025-03-18 15:44:50 +0900

    [level 2] Title: 피로도, Time: 4.45 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,33 +1,23 @@
 import java.util.Arrays;
 class Solution {
-    private int dungeons_count;
-    private long result;
+    public static boolean check[];
+    public static int ans = 0;
 
     public int solution(int k, int[][] dungeons) {
-        dungeons_count = dungeons.length;
-        Boolean[] visited = new Boolean[dungeons_count];
-        Arrays.fill(visited, false);
-        backtracking(k, dungeons, visited);
+        check = new boolean[dungeons.length];
 
-        return (int) result;
+        dfs(k, dungeons, 0);
+
+        return ans;
     }
-
-    public void backtracking(int k, int[][] dungeons, Boolean[] visited) {
-        for (int i = 0; i < dungeons_count; i++) {
-            if (visited[i]) {
-                continue;
-            }
-
-            if (dungeons[i][0] <= k) {
-                Boolean[] arr = Arrays.copyOf(visited, dungeons_count);
-                arr[i] = true;
-                backtracking(k-dungeons[i][1], dungeons, arr);
-
-                long count = Arrays.stream(arr).filter(aBoolean -> aBoolean).count();
-                if (result < count) {
-                    result = count;
-                }
+    public static void dfs(int tired, int[][] dungeons, int cnt){
+        for(int i=0; i<dungeons.length; i++){
+            if(!check[i] && dungeons[i][0]<=tired){
+                check[i] = true;
+                dfs(tired-dungeons[i][1], dungeons, cnt+1);
+                check[i] = false;
             }
         }
+        ans = Math.max(ans, cnt);
     }
 }
