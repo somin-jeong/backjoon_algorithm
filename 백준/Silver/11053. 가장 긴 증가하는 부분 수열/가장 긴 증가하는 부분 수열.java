49b93b24ad35b6178b2f98f9341301a0986c72b8
somin-jeong <81250561+somin-jeong@users.noreply.github.com>
2025-03-26 16:16:31 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 144 ms, Memory: 16176 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,42 +1,41 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int count = Integer.parseInt(br.readLine());
+        int count = Integer.parseInt(bufferedReader.readLine());
+        String line = bufferedReader.readLine();
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[count];
-
+        StringTokenizer st = new StringTokenizer(line, " ");
+        int[] array = new int[count];
         for (int i = 0; i < count; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            array[i] = Integer.parseInt(st.nextToken());
         }
 
         int[] dp = new int[count];
-        for (int i = 0; i < count; i++) {
-            dp[i] = 1;
+        dp[0] = 1;
+        for (int i = 1; i < count; i++) {
             int max = 0;
-            for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i]) {
-                    if (dp[j] > max) {
-                        max = dp[j];
-                    }
+            for (int j = i-1; j >= 0; j--) {
+                if (array[j] < array[i] && dp[j] > max) {
+                    max = dp[j];
                 }
             }
-            dp[i] = max+1;
+            if (max == 0) {
+                dp[i] = 1;
+            } else {
+                dp[i] = max+1;
+            }
         }
 
-        int max = Arrays.stream(dp)
-                .max()
-                .orElse(0);
+        int result = Arrays.stream(dp).max().getAsInt();
+        bufferedWriter.write(result+"");
 
-        bw.write(max + "\n");
-        bw.flush();
-        bw.close();
+        bufferedWriter.flush();
+        bufferedWriter.close();
     }
 }
